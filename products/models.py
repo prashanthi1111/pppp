edb0494a0c89440590926d872188e7f9f5db7c6c
prashanthi1111 <yannamprashanthi98@gmail.com>
2020-10-27 14:52:31 +0530

    commit

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class product(models.Model):
-    title=models.CharField(max_length=50)
-    description=models.CharField(max_length=50)
-    price=models.CharField(max_length=50)
+    title=models.CharField(max_length=120)
+    description=models.TextField(blank=True,null=True)
+    price=models.DecimalField(decimal_places=2,max_digits=10000)
+    summery=models.TextField(blank=True,null=False)
+    featured=models.BooleanField()
